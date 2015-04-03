260ae12ee44f5ce3b64833bd907a3164d7b9acbc
b-mcg <bmcg0890@gmail.com>
2015-04-02 21:22:41 -0700

    Initial commit

--- src/main/scala/com/example/datticker/jsonExtraction/CryptoCoin.scala
+++ src/main/scala/com/example/datticker/jsonExtraction/CryptoCoin.scala
@@ -9,6 +9,27 @@ import scala.util.{Try, Success, Failure}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+/**
+  * __Author__  = bmcg
+  * __Email__   = bmcg0890@gmail.com
+  * __VERSION__ = 0.1 
+  *
+  * __License__ = 
+  * Copyright (C) 2015-2018 b-mcg <bmcg0890@gmail.com>
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  * You should have received a copy of the GNU General Public License
+  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+  *
+  */
+
+
 object CryptoCoin {
 
   type STriple = (String, String, String)
